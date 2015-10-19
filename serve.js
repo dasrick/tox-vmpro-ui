ad7158efe0d5c8c47ca7aab5a088f64ba907fd8c
Enrico Hoffmann <dasrick@gmail.com>
2015-10-19 23:02:00 +0200

    add express and favicon

--- serve.js
+++ serve.js
@@ -1,9 +1,7 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var serve = express();
 serve.use(express.static(__dirname + '/web'));
 serve.set('port', (process.env.PORT || 4000));
 serve.listen(serve.get('port'), function () {
   console.log('Node app of VMPRO is running at localhost:' + serve.get('port'));
-  console.log('Mode: ', process.env.NODE_ENV);
 });
