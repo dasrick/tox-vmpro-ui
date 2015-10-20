a6fcf8f4de5ac9f068eb92dd729bed3a463f347d
Enrico Hoffmann <dasrick@gmail.com>
2015-10-20 07:12:36 +0200

    add webpack incl initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * @ngInject
+ */
+var angular = require('angular');
+
+var requires = [];
+
+angular.module('tox-vmpro-ui-app', requires);
+
+angular.bootstrap(document, ['tox-vmpro-ui-app']);
